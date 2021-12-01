2e21251db166f90698278d2421244275fbd7b8d7
areeh <arehaartveit@gmail.com>
2021-12-01 10:31:50 +0100

    pass up std::io errors

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -2,25 +2,25 @@ pub mod day1 {
     use std::path::Path;
     use std::fs;
 
-    fn read() -> Vec<i32> {
+    fn read() -> std::io::Result<Vec<i32>> {
         let path = Path::new("./src/day1/input.txt");
 
-        fs::read_to_string(path)
-            .expect("Could not read file")
+        let lines = fs::read_to_string(path)?
             .lines()
             .map(|x| x.parse().expect("Could not parse line of file as i32"))
-            .collect()
+            .collect();
+        Ok(lines)
     }
 
     pub fn main1() -> std::io::Result<usize> {
-        let d = read();
+        let d = read()?;
         let num_is_greater = d.windows(2).filter(|w| w[1] > w[0]).count();
 
         Ok(num_is_greater)
     }
 
     pub fn main2() -> std::io::Result<usize> {
-        let d = read();
+        let d = read()?;
         let num_is_greater = d
             .windows(3)
             .collect::<Vec<_>>()
