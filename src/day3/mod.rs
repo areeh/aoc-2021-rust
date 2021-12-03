e38641c35ce830d72a684c906a6ef919ab9d79b0
areeh <arehaartveit@gmail.com>
2021-12-04 00:17:07 +0100

    swap with bit version

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,126 +1,70 @@
-use ndarray::{s, Array2, ArrayView1, ArrayView2, Axis};
-use std::{
-    fs,
-    io::{BufRead, BufReader},
-};
+use std::fs;
 
-fn read_array(path: &str) -> std::io::Result<Array2<u16>> {
-    let reader = BufReader::new(fs::File::open(path)?);
-    let line_len = reader.lines().next().unwrap().unwrap().len();
-
-    let br = BufReader::new(fs::File::open(path)?);
-    let mut data = Vec::new();
-
-    for line in br.lines() {
-        let mut row: Vec<u16> = line
-            .unwrap()
-            .trim()
-            .chars()
-            .map(|v| v.to_digit(2).unwrap() as u16)
-            .collect();
-        data.append(&mut row);
-    }
-
-    let data_len = data.len();
-    let n_rows = data_len / line_len;
-    Ok(Array2::from_shape_vec((n_rows, line_len), data).unwrap())
+fn input1() -> std::io::Result<String> {
+    fs::read_to_string("./src/day3/input.txt")
 }
 
-#[allow(dead_code)]
-fn read_array_str(input: &str) -> std::io::Result<Array2<u16>> {
-    let line_len = input.lines().next().unwrap().len();
-    let mut data = Vec::new();
-
-    for line in input.lines() {
-        let mut row: Vec<u16> = line
-            .trim()
-            .chars()
-            .map(|v| v.to_digit(2).unwrap() as u16)
-            .collect();
-        data.append(&mut row);
-    }
-
-    let data_len = data.len();
-    let n_rows = data_len / line_len;
-    Ok(Array2::from_shape_vec((n_rows, line_len), data).unwrap())
-}
-
-fn to_u32(slice: ArrayView1<bool>) -> u32 {
+fn to_u32(slice: &[bool]) -> u32 {
     slice.iter().fold(0, |acc, &b| acc * 2 + b as u32)
 }
 
-fn to_u32_bin_u16(slice: ArrayView1<u16>) -> u32 {
-    slice.iter().fold(0, |acc, &b| acc * 2 + b as u32)
+fn parse(input: &str) -> Vec<u16> {
+    input
+        .lines()
+        .map(|v| u16::from_str_radix(v.trim(), 2))
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap()
 }
 
-fn part1(arr: ArrayView2<u16>) -> u32 {
-    let half_n_elements = arr.dim().0 / 2;
-    let most_mask = &arr
-        .sum_axis(Axis(0))
-        .mapv(|a| a > half_n_elements.try_into().unwrap());
-    let least_mask = !most_mask;
-    let gamma = to_u32(most_mask.view());
-    let epsilon = to_u32(least_mask.view());
-
-    gamma * epsilon
+fn nth(v: &u16, bits: u32, n: u32) -> u16 {
+    (v & (1 << bits - 1) >> n) >> (bits - 1 - n)
 }
 
-fn part2(arr: ArrayView2<u16>) -> u32 {
-    let mut valid_indices_most: Vec<usize> = (0..arr.dim().0).collect();
-    let mut valid_indices_least = valid_indices_most.to_vec();
+fn sum_nth_bit(bits_slice: &[u16], n: u32, bits: u32) -> u32 {
+    bits_slice
+        .iter()
+        .fold(0, |acc, v| acc + nth(v, bits, n) as u32)
+}
 
-    for i in 0..arr.dim().0 {
-        let mut cnt = 0;
+fn part1(input: &str) -> u32 {
+    let bits = input.lines().next().unwrap().len() as u32;
+    let v = parse(input);
+    let half_n_elements = (v.len() / 2) as u32;
+    let most_mask: Vec<_> = (0..bits)
+        .map(|i| sum_nth_bit(&v, i, bits) > half_n_elements)
+        .collect();
+    let least_mask: Vec<_> = most_mask.iter().map(|v| !v).collect();
+    to_u32(&most_mask) * to_u32(&least_mask)
+}
 
-        for j in &valid_indices_most {
-            if arr[[*j, i]] == 1 {
-                cnt += 1;
-            } else {
-                cnt -= 1;
-            }
-        }
-
-        let most_common = cnt >= 0;
-
-        valid_indices_most.retain(|&j| arr[[j, i]] == most_common as u16);
-        if valid_indices_most.len() == 1 {
+fn some_retain(bit_vec: &[u16], bits: u32, flip: bool) -> u32 {
+    let mut tmp_bit_vec = bit_vec.to_vec();
+    for i in 0..bits {
+        let half_n_elements = tmp_bit_vec.len() as f32 / 2 as f32;
+        let sum_of_nth = sum_nth_bit(&tmp_bit_vec, i, bits);
+        let cmp_val = ((sum_of_nth as f32 >= half_n_elements) ^ flip) as u16;
+        tmp_bit_vec.retain(|&v| nth(&v, bits, i) == cmp_val);
+        if tmp_bit_vec.len() == 1 {
             break;
         }
     }
-    for i in 0..arr.dim().0 {
-        let mut cnt = 0;
+    tmp_bit_vec[0] as u32
+}
 
-        for j in &valid_indices_least {
-            if arr[[*j, i]] == 1 {
-                cnt += 1;
-            } else {
-                cnt -= 1;
-            }
-        }
+fn part2(input: &str) -> u32 {
+    let bits = input.lines().next().unwrap().len() as u32;
+    let bit_vec = parse(input);
 
-        let most_common = cnt >= 0;
+    let oxy = some_retain(&bit_vec, bits, false);
+    let co2 = some_retain(&bit_vec, bits, true);
 
-        valid_indices_least.retain(|&j| arr[[j, i]] == !most_common as u16);
-        if valid_indices_least.len() == 1 {
-            break;
-        }
-    }
-    let oxygen_idx = valid_indices_most[0];
-    let co2_idx = valid_indices_least[0];
-
-    let oxygen_arr = arr.slice(s![oxygen_idx, ..]);
-    let co2_arr = arr.slice(s![co2_idx, ..]);
-
-    let oxygen = to_u32_bin_u16(oxygen_arr.view());
-    let co2 = to_u32_bin_u16(co2_arr.view());
-
-    oxygen * co2
+    (oxy * co2) as u32
 }
 
 pub fn main() -> std::io::Result<()> {
-    let arr = read_array("./src/day3/input.txt")?;
-    println!("{:?}", part1(arr.view()));
-    println!("{:?}", part2(arr.view()));
+    let input = input1()?;
+    println!("{:?}", part1(&input));
+    println!("{:?}", part2(&input));
     Ok(())
 }
 
@@ -138,18 +82,13 @@ fn example() {
     11001
     00010
     01010";
-    assert_eq!(part1(read_array_str(input).unwrap().view()), 198);
-    assert_eq!(part2(read_array_str(input).unwrap().view()), 230);
+    assert_eq!(part1(input), 198);
+    assert_eq!(part2(input), 230);
 }
 
 #[test]
 fn task() {
-    assert_eq!(
-        part1(read_array("./src/day3/input.txt").unwrap().view()),
-        1458194
-    );
-    assert_eq!(
-        part2(read_array("./src/day3/input.txt").unwrap().view()),
-        2829354
-    );
+    let input = input1().unwrap();
+    assert_eq!(part1(&input), 1458194);
+    assert_eq!(part2(&input), 2829354);
 }
